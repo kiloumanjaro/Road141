e9c5abba62d3275b10d19365187f92b69d24fabe
Kint <kiloumanjaro@gmail.com>
2025-06-03 03:49:03 +0800

    initial commit

--- src/entities/portal.js
+++ src/entities/portal.js
@@ -4,7 +4,7 @@ export function makePortal(pos) {
   return k.add([
     k.sprite("portal", { anim: "spin" }),
     k.area({shape: new k.Rect(k.vec2(0), 10, 10)}),
-    k.scale(4),
+    k.scale(5),
     k.anchor("center"),
     k.pos(pos),
     k.offscreen(),
